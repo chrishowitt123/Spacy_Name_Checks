173944a0034bd41f73b62e03dd5e634d005a45c2
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2021-05-15 13:15:33 +0100

    Update name_checks.py

--- name_checks.py
+++ name_checks.py
@@ -1,17 +1,30 @@
+import spacy
 import docx2txt
 import re
-from collections import Counter
 
+nlp = spacy.load("en_core_web_lg")
 
 orignal = r"C:\Users\chris\Documents\Transgola\Clients\PROJECTS\2021\old\393040521_TM_HS\Orignals\ORIGNAL_merge.docx"
-orignal_text = docx2txt.process(orignal)
-
 translation = r"C:\Users\chris\Documents\Transgola\Clients\PROJECTS\2021\old\393040521_TM_HS\Translation\Perfectit\PERFECTIT_merge.docx"
-translation_text = docx2txt.process(translation)
 
-orignal_names = set(re.findall("[A-Z][a-z]+ [A-Z][a-z]+" , orignal_text))
-translation_names = set(re.findall("[A-Z][a-z]+ [A-Z][a-z]+" , translation_text))
+    
+def find_persons_docx_spacy_regex(orignal, translation):
+    
+    orignal_text = docx2txt.process(orignal)
+    orignal_text = orignal_text.replace('\n', ' ').replace('\t', ' ')
+    translation_text = docx2txt.process(translation)
+    translation_text = translation_text.replace('\n', ' ').replace('\t', ' ')
+    
+    doc1 = nlp(orignal_text)
+    doc2 = nlp(translation_text)
+    
+    persons_orignal_sp = [ent.text for ent in doc1.ents if ent.label_ == 'PERSON']
+    persons_translation_sp = [ent.text for ent in doc2.ents if ent.label_ == 'PERSON']
+    spacy_names = sorted(set([n for n in persons_translation_sp if n not in persons_orignal_sp]))
 
-combo = list(orignal_names) + list(translation_names)
-counts = Counter(combo)
-sorted(counts.items())
+    
+    print('SPACY RESULTS')
+    print('\n')
+    
+    for n in spacy_names:
+        print(n)
